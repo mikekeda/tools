ea495a5dac2ba55efa9638937c680eebb12b3e1a
raven <mriynuk@gmail.com>
2015-06-29 20:03:26 +0300

    Added chat app.

---
+++ websocket_server.py
@@ -0,0 +1,46 @@
+from tornado import websocket, web, ioloop
+import json
+
+cl = []
+
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class SocketHandler(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+class ApiHandler(web.RequestHandler):
+
+    @web.asynchronous
+    def get(self, *args):
+        self.finish()
+
+    @web.asynchronous
+    def post(self, *args):
+        self.finish()
+        message = self.get_argument("m")
+        data = {"message": message}
+        data = json.dumps(data)
+        for c in cl:
+            c.write_message(data)
+
+app = web.Application([
+    (r'/', IndexHandler),
+    (r'/ws', SocketHandler),
+    (r'/api', ApiHandler),
+    (r"/static/(.*)", web.StaticFileHandler, {"path": r"./static/"}),
+])
+
+if __name__ == '__main__':
+    app.listen(8080)
+    ioloop.IOLoop.instance().start()
