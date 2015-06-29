c8b5997a5b04dbd5d89450c5c8cf3de33a9fb60a
raven <mriynuk@gmail.com>
2015-06-29 22:09:17 +0300

    Fixed tornado for openshift.

--- websocket_server.py
+++ websocket_server.py
@@ -47,6 +47,7 @@ if __name__ == '__main__':
     if os.environ.get('OPENSHIFT_PYTHON_IP'):
         ip = os.environ.get('OPENSHIFT_PYTHON_IP')
         port = int(os.environ.get('OPENSHIFT_PYTHON_PORT'))
-    app.listen(port, 15000)
+    print port, ip
+    app.listen(15000, ip)
 
     ioloop.IOLoop.instance().start()
