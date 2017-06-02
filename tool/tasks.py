23b194da83d678fd13b796c75117f4a473b875df
Voron <m.keda@internetdevels.com>
2017-06-02 14:03:42 +0300

    Improved send_notification().

--- tool/tasks.py
+++ tool/tasks.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-import requests
+from django.core.mail import send_mail
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from django.utils import timezone
@@ -8,16 +8,19 @@ from schedule.models import Event
 
 @periodic_task(run_every=(crontab(minute='*/15')), name='send_notification', ignore_result=True)
 def send_notification():
-    """Send email notification about events."""
-    # send_mail(
-    #     'Subject here',
-    #     'Here is the message.',
-    #     'site.hackua.com@gmail.com',
-    #     ['mriynuk@gmail.com'],
-    #     fail_silently=False,
-    # )
-    start = timezone.now() + timezone.timedelta(minutes=45)
-    end = timezone.now() + timezone.timedelta(minutes=60)
+    """Send email notification about upcoming events."""
+    interval = 15
+    before = 60
+
+    now = timezone.now()
+    now -= timezone.timedelta(
+        minutes=now.minute % interval,
+        seconds=now.second,
+        microseconds=now.microsecond
+    )
+
+    start = now + timezone.timedelta(minutes=before - interval)
+    end = timezone.now() + timezone.timedelta(minutes=before)
     events = Event.objects.filter(start__gt=start, start__lte=end)
     for event in events:
         name = event.creator.username
@@ -26,13 +29,9 @@ def send_notification():
 
         text = event.description if event.description else 'No description provided'
 
-        requests.post(
-            'https://api.mailgun.net/v3/{}/messages'.format(settings.MAILGUN_SERVER_NAME),
-            auth=('api', settings.MAILGUN_ACCESS_KEY),
-            data={
-                'from': 'Tools site <notify@{}>'.format(settings.MAILGUN_SERVER_NAME),
-                'to': '{} <{}>'.format(name, event.creator.email),
-                'subject': '{} will start today at {}'.format(event.title, event.start.strftime('%H:%M')),
-                'text': text
-            }
+        send_mail(
+            '{} will start today at {}'.format(event.title, event.start.strftime('%H:%M')),
+            text,
+            'Tools site <notify@{}>'.format(settings.MAILGUN_SERVER_NAME),
+            ['{} <{}>'.format(name, event.creator.email)],
         )
