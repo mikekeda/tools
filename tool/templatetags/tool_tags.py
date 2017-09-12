6167a3d870174bac030f10158f79745bce3fd2b5
Mykhailo Keda <mriynuk@gmail.com>
2017-09-12 20:56:58 +0300

    Remove typing from tool_tags.py

--- tool/templatetags/tool_tags.py
+++ tool/templatetags/tool_tags.py
@@ -1,8 +1,6 @@
-from typing import Any
-
 from django.template.defaulttags import register
 
 
 @register.filter
-def get_item(obj, key: str) -> Any:
+def get_item(obj, key: str):
     return obj.get(str(key)) if isinstance(obj, dict) else getattr(obj, str(key))
