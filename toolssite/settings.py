5f9533e27e288db6c00104eb6a48ff993dd4802d
Mykhailo Keda <mriynuk@gmail.com>
2017-10-10 23:09:33 +0300

    Improved get_env_var().

--- toolssite/settings.py
+++ toolssite/settings.py
@@ -14,13 +14,18 @@ def get_env_var(name, default=''):
     """Get all sensitive data from google vm custom metadata."""
     try:
         name = '_'.join([SITE_ENV_PREFIX, name])
-        res = requests.get(
-            'http://metadata.google.internal/computeMetadata/'
-            'v1/instance/attributes/{}'.format(name),
-            headers={'Metadata-Flavor': 'Google'}
-        )
-        if res.status_code == 200:
-            return res.text
+        res = os.environ.get(name)
+        if res:
+            # Check env variable (Jenkins build).
+            return res
+        else:
+            res = requests.get(
+                'http://metadata.google.internal/computeMetadata/'
+                'v1/instance/attributes/{}'.format(name),
+                headers={'Metadata-Flavor': 'Google'}
+            )
+            if res.status_code == 200:
+                return res.text
     except requests.exceptions.ConnectionError:
         return default
     return default
