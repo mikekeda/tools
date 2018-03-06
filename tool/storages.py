f10ce305ee41b52b918d0337473467db3f960f94
Mykhailo Keda <mkeda@softserveinc.com>
2018-03-06 18:25:09 +0200

    Implemented abstract method get_created_time for ToolStorage Class.

--- tool/storages.py
+++ tool/storages.py
@@ -1,5 +1,7 @@
-from storages.backends.gcloud import GoogleCloudStorage
 from django.conf import settings
+from django.utils import timezone
+from storages.backends.gcloud import GoogleCloudStorage
+from storages.utils import clean_name
 
 
 class ToolStorage(GoogleCloudStorage):
@@ -7,3 +9,12 @@ class ToolStorage(GoogleCloudStorage):
         """ Normalizes the name. """
         return '{}/{}'.format(settings.SITE_ENV_PREFIX.lower(),
                               super()._normalize_name(name))
+
+    def get_created_time(self, name):
+        """
+        Return the creation time (as a datetime) of the file specified by name.
+        The datetime will be timezone-aware if USE_TZ=True.
+        """
+        name = self._normalize_name(clean_name(name))
+        blob = self._get_blob(self._encode_name(name))
+        return timezone.make_naive(blob.time_created)
