e562380212d6ed978e56a65de56b193b9cbb825a
Mykhailo Keda <mriynuk@gmail.com>
2020-06-01 19:20:13 +0200

    Moved Fake news check to anonymous tool.

--- tool/sitemaps.py
+++ tool/sitemaps.py
@@ -1,7 +1,7 @@
 from django.contrib import sitemaps
 from django.urls import reverse
 
-from .context_processors import categories
+from tool.context_processors import categories
 
 
 class StaticViewSitemap(sitemaps.Sitemap):
@@ -9,9 +9,9 @@ class StaticViewSitemap(sitemaps.Sitemap):
     changefreq = 'weekly'
 
     def items(self):
-        return [tool['slug'] for tool in categories()['tools']] + ['about']
+        return [tool['slug'] for tool in categories()['tools']] + ['about_page', 'news_check']
 
     def location(self, obj):
-        if obj == 'about':
-            return reverse('about_page')
+        if obj in {'about_page', 'news_check'}:
+            return reverse(obj)
         return reverse('tool', kwargs={'slug': obj})
